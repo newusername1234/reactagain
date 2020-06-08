6cb4ab1761bab26d12ef3502094b66b2cf4920f2
Wes <wesleyayer92@gmail.com>
2020-06-08 14:05:53 -0400

    add some props styling

--- src/App.js
+++ src/App.js
@@ -3,12 +3,30 @@ import './App.css';
 import Card from './components/Card';
 
 function App() {
+  const projects = [
+    {name: "Project Awesome", likes: 1},
+    {name: "Project Awesomest", likes: 2},
+    {name: "The REAL project awesome", likes: 3},
+    {name: "The real real blah blah", likes: 4},
+    {name: "a new project wow", likes: 5},
+    {name: "another one look at this!", likes: 6},
+  ];
+
+  const colors = ["blue", "red", "green", "purple"];
+
+  const myStyle = {
+    maxWidth: "30%", 
+    borderRadius: "5px", 
+    boxShadow: "0px 3px 5px rgba(0, 0, 0, 0.5)", 
+    padding: "10px", 
+    margin: "10px",
+  };
+
   return (
     <div className="App">
-      <Card name="Project Awesome" likes={0} />
-      <Card name="Project Awesomest" likes={1} />
-      <Card name="The REAL project awesome" likes={2} />
-      <Card />
+      {projects.map((project, i) => {
+        return <Card style={{...myStyle, backgroundColor: colors[i % colors.length]}} name={project.name} key={i} likes={project.likes} />
+      })}
     </div>
   );
 }
